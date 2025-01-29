51d230c513c7633284efa8e9d3df47a02c3ee447
Cristian Fernandez <cfernandez.programacion@gmail.com>
2025-01-28 23:15:18 -0300

    Actualizar la instancia de MongoStore en app.js para utilizar MongoStore.create y simplificar la gestión de sesiones con MongoDB

--- compuservice-api/src/app.js
+++ compuservice-api/src/app.js
@@ -6,7 +6,7 @@ import { addLogger, appLogger } from './config/loggers.config.js';
 import { __dirname } from './path.js';
 import indexRouter from './routes/index.routes.js';
 import dotenv from 'dotenv';
-import connectMongo from 'connect-mongo';
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -20,44 +20,34 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
-// Instanciar la clase MongoStore correctamente con 'new'
-const MongoStoreInstance = new MongoStore({
-  mongooseConnection: MongoSingleton.getInstance(),  
-});
-
-async function connectMongoDB() {
-  appLogger.info('Connecting to MongoDB...');
-  try {
-    const mongooseConnection = await MongoSingleton.getInstance(); // Esperamos que la conexión esté lista
-    app.use(
-      session({
-        secret: process.env.SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStoreInstance,  // Usamos la instancia de MongoStore
-        cookie: {
-          httpOnly: true,
-          secure: false,
-          maxAge: 24 * 1000 * 60 * 60, // 24 horas
-        },
-      })
-    );
-    appLogger.info('MongoDB connected successfully');
-  } catch (error) {
-    appLogger.error('MongoDB connection error: ', error);
-    process.exit(1);  // Detenemos la aplicación si no se conecta correctamente
-  }
-}
-
-// Llamar a la función para conectar a MongoDB
-connectMongoDB();
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: config.mongoUrl
+    }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 1000 * 60 * 60,
+    },
+  })
+);
 
 app.use(addLogger);
 
 app.use(indexRouter);
 
-const port = process.env.PORT || 4000;
+async function connectMongo() {
+  appLogger.info('Connecting to MongoDB...');
+  try {
+    await MongoSingleton.getInstance()
+  } catch (error) {
+    appLogger.error('MongoDB connection error: ', error);
+    process.exit(1);
+  }
+}
 
-app.listen(port, () => {
-  appLogger.info(`Server running on port ${port}`);
-});
+connectMongo();
